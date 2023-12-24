e7fa6d54b7f2b84cf850fa15984e4ffd2cfc17c4
Jonas Carpay <jonas.carpay@ndhtrading.com>
2023-12-24 15:06:19 +0800

    Simplify template

--- template/flake.nix
+++ template/flake.nix
@@ -1,22 +1,19 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     mkflake.url = "github:jonascarpay/mkflake";
   };
 
-  outputs = { nixpkgs, mkflake, ... }: mkflake.lib.mkflake {
-    toplevel = {
-      # top-level outputs, such as library functions and
-      # overlays go here
-    };
+  outputs = { nixpkgs, mkflake, self }: mkflake.lib.mkflake {
     perSystem = system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        # per-system outputs, such as devShells and packages go here
         devShells.default = pkgs.mkShell {
-          packages = [ pkgs.hello ];
+          packages = [
+            pkgs.hello
+          ];
         };
       };
   };
